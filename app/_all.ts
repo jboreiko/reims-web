0370be51e6637b2e9679f63938b7efa7e4175b46
John Boreiko <john.boreiko@gmail.com>
2016-02-25 00:15:16 +0000

    Updated ingest to mark glasses as filed as well as update home to have plottable charts

--- app/_all.ts
+++ app/_all.ts
@@ -1,3 +1,5 @@
 /// <reference path='libs/jquery/jquery.d.ts' />
 /// <reference path='libs/angularjs/angular.d.ts' />
 /// <reference path='libs/pouchDB/pouch.d.ts' />
+/// <reference path='libs/plottable/plottable.d.ts' />
+/// <reference path='libs/d3/d3.d.ts' />
--- app/controllers.ts
+++ app/controllers.ts
@@ -102,6 +102,32 @@ reimsControllers.controller(
     "HomeCtrl", ["$scope", "EyeglassRecords",
 		 function($scope, EyeglassRecords) {
 		     console.log("Home controller", EyeglassRecords);
+
+		     var data = [];
+		     EyeglassRecords.localAllDocs({include_docs : true}).then(function(results) {
+			 angular.forEach(results.rows, function(row) {
+			     console.log(row);
+			 });
+		       }).catch(function(err) {
+			   console.log(err);
+		       });
+
+
+		     var scale = new Plottable.Scales.Linear();
+		     var colorScale = new Plottable.Scales.InterpolatedColor();
+		     colorScale.range(["#BDCEF0", "#5279C7"]);
+		     //var data = [{ val: 1 }, { val: 2 }, { val: 3 },
+		     // { val: 4 }, { val: 5 }, { val: 6 }];
+
+		     var plot = new Plottable.Plots.Pie()
+			 .addDataset(new Plottable.Dataset(data))
+			 .sectorValue(function(d) { return d.val; }, scale)
+			 .attr("fill", function(d) { return d.val; }, colorScale)
+			 .renderTo("svg#example");
+
+		     window.addEventListener("resize", function() {
+			 plot.redraw();
+		     });
 		 }]);
 
 reimsControllers.controller(
