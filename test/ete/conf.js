a6b028bc616a4aa669f9cb21f75e67dc43b503ad
John Boreiko <john.boreiko@gmail.com>
2016-03-15 21:52:28 -0700

    Reenable firefox testing for ete

--- test/ete/conf.js
+++ test/ete/conf.js
@@ -2,7 +2,9 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['login-spec.js'],
     framework: 'jasmine2',
-    capabilities: {
+    multiCapabilities: [{
+	browserName: 'firefox'
+    }, {
 	browserName: 'chrome'
-    }
+    }]
 };
