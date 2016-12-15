28bfc1de1aa825443cbfb6d9be0735fad830c87a
John Boreiko <john.boreiko@gmail.com>
2016-12-15 00:11:15 +0000

    Switch all files from tabs to spaces

--- app/navBar/navBar.ts
+++ app/navBar/navBar.ts
@@ -4,32 +4,32 @@ module reimsApp.NavBar {
     "use strict";
 
     function navBar(): ng.IDirective {
-	return {
-	    restrict: "E",
-	    controller: "NavBarCtrl",
-	    controllerAs: "Ctrl",
-	    templateUrl: "partials/navBar.html"
-	};
+    return {
+        restrict: "E",
+        controller: "NavBarCtrl",
+        controllerAs: "Ctrl",
+        templateUrl: "partials/navBar.html"
+    };
     };
 
     class NavBarController {
-	public static $inject = ["$scope"];
-	isCollapsed: boolean;
+    public static $inject = ["$scope"];
+    isCollapsed: boolean;
 
-	constructor(private $scope: ng.IScope) {
-	    this.isCollapsed = true;
-	}
+    constructor(private $scope: ng.IScope) {
+        this.isCollapsed = true;
+    }
 
-	toggleCollapse(): void {
-	    this.isCollapsed = !this.isCollapsed;
-	}
+    toggleCollapse(): void {
+        this.isCollapsed = !this.isCollapsed;
+    }
 
-	collapse(): void {
-	    this.isCollapsed = true;
-	}
+    collapse(): void {
+        this.isCollapsed = true;
+    }
     }
 
     var reimsDirectives = angular.module("reimsApp.NavBar", [])
-	.directive("reimsNavBar", navBar)
-	.controller("NavBarCtrl", NavBarController);
+    .directive("reimsNavBar", navBar)
+    .controller("NavBarCtrl", NavBarController);
 }
--- app/search/search.ts
+++ app/search/search.ts
@@ -4,95 +4,95 @@ module reimsApp.Search {
     "use strict";
 
     class SearchController {
-	public static $inject = ["$scope", "$uibModal", "$filter", "ngToast", "EyeglassRecords"];
+    public static $inject = ["$scope", "$uibModal", "$filter", "ngToast", "EyeglassRecords"];
 
-	constructor (private $scope: any,
-		     private $uibModal: ng.ui.bootstrap.IModalService,
-		     private $filter: any,
-		     private toast: any,
-		     private EyeglassRecords: any) {
-	    console.log("Search controller");
+    constructor (private $scope: any,
+             private $uibModal: ng.ui.bootstrap.IModalService,
+             private $filter: any,
+             private toast: any,
+             private EyeglassRecords: any) {
+        console.log("Search controller");
 
-	    $scope.searchTerms = {};
-	    $scope.fullSearchResults = [];
-	    $scope.displayFull = false;
+        $scope.searchTerms = {};
+        $scope.fullSearchResults = [];
+        $scope.displayFull = false;
 
-	    this.updateQuickResults();
+        this.updateQuickResults();
 
-	    this.$scope.onSubmit = {
-		name: "Full Search",
-		func: function(valid: boolean, error: any, terms: any) {
-		    // ignore validity of form
-		    $scope.fullSearchResults = [{doc: {sku: "success"}}];
-		}
-	    };
-	}
+        this.$scope.onSubmit = {
+        name: "Full Search",
+        func: function(valid: boolean, error: any, terms: any) {
+            // ignore validity of form
+            $scope.fullSearchResults = [{doc: {sku: "success"}}];
+        }
+        };
+    }
 
-	updateQuickResults() {
-	    this.EyeglassRecords.getByStatus("filed").then((results) => {
-		this.$scope.allResultRows = results.rows;
-	    });
-	}
+    updateQuickResults() {
+        this.EyeglassRecords.getByStatus("filed").then((results) => {
+        this.$scope.allResultRows = results.rows;
+        });
+    }
 
-	search(searchTerms: any): void {
-	    console.log("Running a search with ", searchTerms);
-	};
+    search(searchTerms: any): void {
+        console.log("Running a search with ", searchTerms);
+    };
 
-	dispense(): void {
-	    var rows = this.getSelectedRows();
-	    if (rows.length === 0) {
-		this.toast.warning("Please select rows first");
-		return;
-	    }
+    dispense(): void {
+        var rows = this.getSelectedRows();
+        if (rows.length === 0) {
+        this.toast.warning("Please select rows first");
+        return;
+        }
 
-	    console.log("Dispensing", rows);
-	    var action: Modals.IModalAction = {"name" : "dispense",
-					       "rows" : rows,
-  					       "success" : () => {
-						   this.EyeglassRecords.updateRowsStatus(rows, "dispense");
-					       }};
-	    Modals.openModal(this.$uibModal, action);
-	};
+        console.log("Dispensing", rows);
+        var action: Modals.IModalAction = {"name" : "dispense",
+                           "rows" : rows,
+                             "success" : () => {
+                           this.EyeglassRecords.updateRowsStatus(rows, "dispense");
+                           }};
+        Modals.openModal(this.$uibModal, action);
+    };
 
-	markAsMissing(): void {
-	    var rows = this.getSelectedRows();
-	    if (rows.length === 0) {
-		this.toast.warning("Please select rows first");
-		return;
-	    }
+    markAsMissing(): void {
+        var rows = this.getSelectedRows();
+        if (rows.length === 0) {
+        this.toast.warning("Please select rows first");
+        return;
+        }
 
-	    console.log("Marking as missing", rows);
-	    var action: Modals.IModalAction = {"name" : "mark as missing",
-					       "rows" : rows,
-					       "success": () => {
-						   this.EyeglassRecords.updateRowsStatus(rows, "missing");
-					       }};
+        console.log("Marking as missing", rows);
+        var action: Modals.IModalAction = {"name" : "mark as missing",
+                           "rows" : rows,
+                           "success": () => {
+                           this.EyeglassRecords.updateRowsStatus(rows, "missing");
+                           }};
 
-	    Modals.openModal(this.$uibModal, action);
-	};
+        Modals.openModal(this.$uibModal, action);
+    };
 
-	update(): void {
-	    var rows = this.getSelectedRows();
-	    console.log("Update row", rows);
-	    this.toast.danger("Update not available");
-	};
+    update(): void {
+        var rows = this.getSelectedRows();
+        console.log("Update row", rows);
+        this.toast.danger("Update not available");
+    };
 
-	selectRow(row: any): void {
-	    console.log("Toggling", row.id);
-	    row.selected = row.selected ? false : true;
-	};
+    selectRow(row: any): void {
+        console.log("Toggling", row.id);
+        row.selected = row.selected ? false : true;
+    };
 
-	showFull(): void {
-	    this.$scope.displayFull = true;
-	};
+    showFull(): void {
+        this.$scope.displayFull = true;
+    };
 
-	showQuick(): void {
-	    this.$scope.displayFull = false;
-	};
+    showQuick(): void {
+        this.$scope.displayFull = false;
+    };
 
-	private getSelectedRows() {
-	    return this.$filter("filter")(this.$scope.allResultRows, {selected: true}, true);
-	}
+    private getSelectedRows() {
+        return this.$filter("filter")(this.$scope.allResultRows, {selected: true}, true);
+    }
     }
 
     var app = angular.module("reimsApp.Search", []);
